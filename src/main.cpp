6d52834e41b503371d868118af1d28f7db53aca4
Martyn Clifford <m.a.clifford2@gmail.com>
2024-05-21 11:03:58 +0100

    Update NameUpdate TestAdd project files.

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello my 3d worl", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hello my 3d world", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
